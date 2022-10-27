51ea48cebebc8611baa83b1f9f8fd6a11ff30d4c
Satorrid <Devintsefton@gmail.com>
2022-10-27 13:36:28 -0700

    remove node modules

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./user-api");
+const thoughtRoutes = require("./thoughts-api");
 
 router.use("/user", userRoutes);
+router.use("/thought", thoughtRoutes);
 
 router.use((req, res) => res.send("Wrong Route!"));
 
